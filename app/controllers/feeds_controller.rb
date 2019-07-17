d6c485914949e0cca15e9aff3743ff33b67e708f
Firose <firoseiqbal123@gmail.com>
2019-07-17 18:49:14 +0530

    New Update

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -25,7 +25,7 @@ class FeedsController < ApplicationController
   # POST /feeds.json
   def create
     @feed = Feed.new(feed_params)
-
+    create_entries(@feed)
     respond_to do |format|
       if @feed.save
         format.html { redirect_to @feed, notice: 'Feed was successfully created.' }
@@ -42,6 +42,8 @@ class FeedsController < ApplicationController
   def update
     respond_to do |format|
       if @feed.update(feed_params)
+        @feed.entries.delete_all
+        create_entries(@feed)
         format.html { redirect_to @feed, notice: 'Feed was successfully updated.' }
         format.json { render :show, status: :ok, location: @feed }
       else
@@ -54,6 +56,7 @@ class FeedsController < ApplicationController
   # DELETE /feeds/1
   # DELETE /feeds/1.json
   def destroy
+    @feed.entries.delete_all
     @feed.destroy
     respond_to do |format|
       format.html { redirect_to feeds_url, notice: 'Feed was successfully destroyed.' }
@@ -70,5 +73,14 @@ class FeedsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
       params.require(:feed).permit(:name, :url, :description)
+    end
+
+    def create_entries(feed)
+      xml = HTTParty.get(feed.url).body
+      content = Feedjira.parse(xml)
+      content.entries.each do |entry|
+        local_entry = feed.entries.where(title: entry.title).first_or_initialize
+        local_entry.update_attributes(content: entry.summary, author: entry.author, url: entry.url, published: entry.published)
+      end
     end
 end
